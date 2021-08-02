6b96e72754073cc775b8a28073e099bc7d8f2675
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-01 20:08:19 -0700

    Initial Commit

---
+++ AnimatingBindings/AnimatingBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingBindingsApp.swift
+//  AnimatingBindings
+//
+//  Created by RachelleLee on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
